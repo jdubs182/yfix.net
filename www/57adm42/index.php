194c10259c705ddedd9f1c38605bbea3a2c82c63
Yuri Vysotskiy <profy.net@gmail.com>
2014-06-03 23:29:21 +0300

    moved configs into app_path/config/

--- www/57adm42/index.php
+++ www/57adm42/index.php
@@ -1,9 +1,9 @@
 <?php
-$dev_settings = dirname(dirname(dirname(__FILE__))).'/.dev/override.php';
+$dev_settings = dirname(dirname(__DIR__)).'/.dev/override.php';
 if (file_exists($dev_settings)) {
     require_once $dev_settings;
 }
-$saved_settings = dirname(dirname(__FILE__)).'/saved_settings.php';
+$saved_settings = dirname(dirname(__DIR__)).'/config/saved_settings.php';
 if (file_exists($saved_settings)) {
     require_once $saved_settings;
 }
@@ -15,6 +15,6 @@ define('ADMIN_WEB_PATH', WEB_PATH. basename(dirname(__FILE__)).'/');
 define('ADMIN_SITE_PATH', dirname(__FILE__).'/');
 define('SITE_DEFAULT_PAGE', './?object=admin_home');
 define('ADMIN_FRAMESET_MODE', 1);
-require dirname(dirname(__FILE__)).'/project_conf.php';
+require dirname(dirname(__DIR__)).'/config/project_conf.php';
 require YF_PATH.'classes/yf_main.class.php';
 new yf_main('admin', $no_db_connect = false, $auto_init_all = true);
--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <?php
-$dev_settings = dirname(dirname(__FILE__)).'/.dev/override.php';
+$dev_settings = dirname(__DIR__).'/.dev/override.php';
 if (file_exists($dev_settings)) {
     require_once $dev_settings;
 }
-$saved_settings = dirname(__FILE__).'/saved_settings.php';
+$saved_settings = dirname(__DIR__).'/config/saved_settings.php';
 if (file_exists($saved_settings)) {
     require_once $saved_settings;
 }
@@ -12,7 +12,7 @@ define('YF_PATH', '/home/www/yf/');
 define('WEB_PATH', '//yfix.net/');
 define('SITE_DEFAULT_PAGE', './?object=docs');
 define('SITE_ADVERT_NAME', 'Yfix.net');
-require dirname(__FILE__).'/project_conf.php';
+require dirname(__DIR__).'/config/project_conf.php';
 $PROJECT_CONF['tpl']['REWRITE_MODE'] = true;
 require YF_PATH.'classes/yf_main.class.php';
 new yf_main('user', $no_db_connect = false, $auto_init_all = true);
