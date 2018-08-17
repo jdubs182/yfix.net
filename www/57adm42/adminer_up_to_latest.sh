d2b42897d2ed4929a3a35ec1834b19bed940c3ad
Yfix dev <yfix.dev@gmail.com>
2018-08-17 17:24:05 +0300

    adminer up

--- www/57adm42/adminer_up_to_latest.sh
+++ www/57adm42/adminer_up_to_latest.sh
@@ -1,1 +1,3 @@
-wget "http://www.adminer.org/latest-mysql-en.php" -O adminer.php
+#wget "http://www.adminer.org/latest-mysql-en.php" -O adminer.php
+#wget https://raw.githubusercontent.com/xDmv/adminer/master/my_adminer.php -O adminer.php
+wget https://github.com/yfix/adminer/releases/download/v4.6.3-yfix/adminer-4.6.3-yfix.php -O adminer.php
